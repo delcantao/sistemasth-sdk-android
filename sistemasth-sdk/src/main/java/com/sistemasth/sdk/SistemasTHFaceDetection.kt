07ffccc40733d1b389156adb99b028932c78180c
Fernando del Cantão <feedc91@gmail.com>
2022-08-04 17:56:40 +0200

    adding properties

--- sistemasth-sdk/src/main/java/com/sistemasth/sdk/SistemasTHFaceDetection.kt
+++ sistemasth-sdk/src/main/java/com/sistemasth/sdk/SistemasTHFaceDetection.kt
@@ -2,12 +2,7 @@ package com.sistemasth.sdk
 
 import androidx.annotation.Keep
 import androidx.annotation.StyleRes
-import androidx.compose.runtime.Composable
 import br.com.nxcd.facedetection.NxcdFaceDetection
-import com.google.android.gms.common.annotation.KeepName
-import com.google.android.gms.common.internal.safeparcel.SafeParcelable
-import com.google.gson.annotations.Expose
-import dagger.Module
 
 @Keep
 class SistemasTHFaceDetection : NxcdFaceDetection {
@@ -15,14 +10,14 @@ class SistemasTHFaceDetection : NxcdFaceDetection {
     }
 
     constructor(i: Int, s: String?, @StyleRes res: Int) : super(i, s, res) {}
-}
-class Teste {
+
     @Keep
-    fun Teste():String{
-        return "Teste";
-    }
-}
-@KeepName
-fun Teste(t: String): String {
-    return t + "Hello";
+    val RESULT = "result"
+
+    @Keep
+    val THROWABLE = "throwable"
+
+    @Keep
+    val IMAGE_RESULT = "image_result"
+
 }
